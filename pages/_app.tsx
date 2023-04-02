9f9b9a13ea1dcad906fe546086f3a22caf4ae852
Matthieudegny <matthieu.dev.degny@gmail.com>
2023-04-02 15:46:36 +0200

    test release 2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,13 @@
 import "@/styles/app.scss";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence } from "framer-motion";
 
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
-import Head from "next/head";
 
-export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const pageKey = router.asPath;
-
+export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <div className="test_app">
       <AnimatePresence initial={false}>
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
-          />
-        </Head>
-        <Component key={pageKey} {...pageProps} />
+        <Component key={router.pathname} {...pageProps} />
       </AnimatePresence>
     </div>
   );
