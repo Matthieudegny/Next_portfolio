22932f2d84d239a5b2f83a9149c6900386af242a
Matthieudegny <matthieu.dev.degny@gmail.com>
2023-04-02 17:03:12 +0200

    fixe bugg release

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <div className="test_app">
-      <AnimatePresence initial={false}>
+      <AnimatePresence initial={false} mode="popLayout">
         <Component key={router.pathname} {...pageProps} />
       </AnimatePresence>
     </div>
