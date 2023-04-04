df40d3360a4b845c2b055f309011281d89ab1b1b
Matthieudegny <matthieu.dev.degny@gmail.com>
2023-04-04 15:37:46 +0200

    test relaese v3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,11 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
-    <div className="font-playfair relative h-screen overflow-hidden py-24 px-12 lg:px-48">
-      <AnimatePresence initial={false} mode="popLayout">
+    <div className="test_app">
+      <AnimatePresence
+        initial={false}
+        onExitComplete={() => window.scrollTo(0, 0)}
+      >
         <Component key={router.pathname} {...pageProps} />
       </AnimatePresence>
     </div>
