463b85a61049e8a09bfe6bb865aa4e95cc62e26a
Matthieudegny <matthieu.dev.degny@gmail.com>
2023-04-25 21:19:12 +0200

    nodemailer installed

---
+++ pages/api/contact.ts
@@ -0,0 +1,48 @@
+import { NextApiRequest, NextApiResponse } from "next";
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+interface ErrorResponse extends Error {
+  address?: string;
+}
+interface infoResponse extends Error {
+  accepted?: string;
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log(req.body);
+  try {
+    const message = {
+      from: req.body.email,
+      to: process.env.GMAIL_EMAIL_ADDRESS,
+      subject: req.body.subject,
+      text: req.body.message,
+      html: `<p>${req.body.message}</p>`,
+    };
+
+    let transporter = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: process.env.GMAIL_EMAIL_ADDRESS,
+        pass: process.env.GMAIL_APP_PASSWORD,
+      },
+    });
+
+    transporter.sendMail(message, (err: ErrorResponse, info: infoResponse) => {
+      if (err) {
+        res.status(404).json({
+          error: `Connection refused at ${err.address}`,
+        });
+      } else {
+        res.status(250).json({
+          success: `Message delivered to ${info.accepted}`,
+        });
+      }
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      error: err.message,
+    });
+    console.log(err.message);
+  }
+}
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -26,6 +26,65 @@ function useFollowPointer(ref: RefObject<HTMLElement>) {
   return point;
 }
 
+interface FormData {
+  name: string;
+  email: string;
+  message: string;
+}
+
+function ContactForm() {
+  const [formData, setFormData] = useState<FormData>({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("/api/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) {
+        throw new Error("There was an error sending your message. Please try again later.");
+      }
+
+      console.log(await response.json());
+      alert("Your message has been sent!");
+    } catch (error) {
+      console.error(error);
+      alert("There was an error sending your message. Please try again later.");
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input type="text" name="name" value={formData.name} onChange={handleChange} />
+      </label>
+      <label>
+        Email:
+        <input type="email" name="email" value={formData.email} onChange={handleChange} />
+      </label>
+      <label>
+        Message:
+        <textarea name="message" value={formData.message} onChange={handleChange} />
+      </label>
+      <button type="submit">Send</button>
+    </form>
+  );
+}
 const Contact = () => {
   const [point, setPoint] = useState({ x: 0, y: 0 });
 
@@ -49,9 +108,9 @@ const Contact = () => {
       className="fixed top-0 left-0 w-full h-screen  overflow-y-auto"
     >
       <main className="relative bg-primary-color text-gray-900 min-h-screen w-screen  font-Montserrat_regular">
-        <motion.div
+        {/* <motion.div
           ref={ref}
-          className="w-20 h-20 bg-gray-900 z-50"
+          className="w-20 h-20 bg-gray-900  rounded-full "
           animate={customAnimationProps}
           transition={{
             type: "spring",
@@ -59,8 +118,8 @@ const Contact = () => {
             stiffness: 300,
             restDelta: 0.001,
           }}
-        />
-
+        /> */}
+        <ContactForm />
         <motion.h1>Contact</motion.h1>
         <Link href="/">Home</Link>
       </main>
