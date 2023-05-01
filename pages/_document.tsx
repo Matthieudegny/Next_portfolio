a2f86aacc826f5217c477b74de11c79ed3647f87
Matthieudegny <matthieu.dev.degny@gmail.com>
2023-05-01 19:22:49 +0200

    change photo brwoser

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" type="image/ico" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/photo.png" />
       </Head>
       <body>
         <Main />
