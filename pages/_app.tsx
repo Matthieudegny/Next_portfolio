1a4ba2a3118fc843a4ec7ed4a147b664a12fe24b
Matthieudegny <matthieu.dev.degny@gmail.com>
2023-05-06 16:15:51 +0200

    test meta data in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/app.css";
 import { AnimatePresence } from "framer-motion";
 import React, { useEffect, useState } from "react";
+import Head from "next/head";
 
 import Nav from "../components/Nav/Nav";
 
@@ -35,6 +36,10 @@ export default function App({ Component, pageProps, router }: AppProps) {
 
   return (
     <div className="relative min-h-screen h-full overflow-y-auto py-24 px-12 lg:px-48">
+      <Head>
+        <title>Matthieu DEGNY Portfolio</title>
+        <meta name="Matthieu DEGNY Portfolio" content="Matthieu DEGNY Portfolio" />
+      </Head>
       {/* initial false => at the mount of the component no animation */}
       <AnimatePresence initial={false}>
         <Nav
